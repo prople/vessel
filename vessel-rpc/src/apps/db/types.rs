7a67e3fbe68c994dc0a96ece82f9229da2089da6
rstlix0x0 <rstlix.dev@gmail.com>
2024-07-14 23:07:47 +0700

    refactor(vessel-rpc): remove internal instruction enum

--- vessel-rpc/src/apps/db/types.rs
+++ vessel-rpc/src/apps/db/types.rs
@@ -4,8 +4,4 @@ use rst_common::with_errors::thiserror::{self, Error};
 pub enum DbError {
     #[error("bucket error: {0}")]
     BucketError(String),
-
-    #[error("instruction error: {0}")]
-    #[allow(dead_code)]
-    InstructionError(String),
 }
