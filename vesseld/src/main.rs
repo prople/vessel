8da1f61232c5295a25191277a20ad025121401a5
rstlix0x0 <rstlix.dev@gmail.com>
2024-09-02 09:48:12 +0700

    feat(vesseld): adding and setup clap

--- vesseld/src/main.rs
+++ vesseld/src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "vesseld")]
+#[command(version = "1.0")]
+#[command(long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    if let Some(conf) = cli.config.as_deref() {
+        let out = conf.to_str();
+        println!("config value: {}", out.unwrap())
+    }
 }
